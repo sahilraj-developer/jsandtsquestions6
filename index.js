4f27f8c323e9f9d4d7eaf1070651fbd1e949a2b6
sahil <sahil27raj@gmail.com>
2024-04-28 12:55:20 +0530

    add the solution for question number 6

---
+++ index.js
@@ -0,0 +1,16 @@
+// Write a function called isPalindrome that takes a string as input and returns true if the string is a palindrome,
+//  and false otherwise. A palindrome is a word, phrase, number,or other sequence of characters that reads the same 
+//  forward and backward, ignoring spaces, punctuation, and capitalization.
+
+
+// isPalindrome("racecar"); // Expected output: true
+// isPalindrome("level"); // Expected output: true
+// isPalindrome("hello"); // Expected output: false
+// isPalindrome("A man, a plan, a canal, Panama"); // Expected output: true
+
+
+
+const isPalindrome=(str)=>{
+    return str.split('').reverse().join('') === str;
+}
+isPalindrome("level")
